966bcff4f5980b53687a87ecc23b296fa7b54efa
RiveraDenisse <denisse_alejandrar@hotmail.com>
2021-09-17 13:17:32 -0500

    added the object store

--- Develop/public/js/idb.js
+++ Develop/public/js/idb.js
@@ -3,3 +3,27 @@
 let db;
 //establish a connection to IndexedDB database called budget_tracker and set to version 1
 const request = indexedDB.open("budget_tracker", 1);
+
+//this event will emit if the database version changes
+request.onupgradeneeded = function (event) {
+  //save a reference to the database
+  const db = event.target.result;
+  //create an object store, set it to have an auto incrementing primary key
+  db.createObjectStore("new_transaction", { autoIncrement: true });
+};
+
+//upon a successful
+request.onsuccess = function (event) {
+  //when db is successfully create with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
+  db.event.target.result;
+
+  //check if app is online, if yest run uploadTransaction() to send all local db data to api
+  if (navigator.onLine) {
+    uploadTransaction();
+  }
+};
+
+request.onerror = function (event) {
+  //log error here
+  console.log(event.target.errorCode);
+};
